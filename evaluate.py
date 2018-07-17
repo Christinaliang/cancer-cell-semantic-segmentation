7f6c45e5735bd3e43bef56ccf1689a3fd9ef210e
Accelerator <wc2476@columbia.edu>
2018-07-17 11:54:26 +0800

    Add argparser

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,9 @@
-import time
-import argparse
 import os
+import argparse
+import time
+
+import numpy as np
+import math
 
 from models import *
 from celldataset import CellImages
@@ -11,33 +14,27 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.backends import cudnn
 
-import numpy as np
-import math
-
 
 def main():
-    is_resume = True
+    parser = argparse.ArgumentParser(description='Model Evaluation')
+    parser.add_argument('--bs', default=16, type=int, help='Batch size')
+    parser.add_argument('--cpus', default=8, type=int, help='The number of CPUs for loading data')
+    parser.add_argument('--savepred', action='store_true', help='Save predicted masks')
+    args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print('Device is {}!'.format(device))
-    best_acc = 0.
-
-    # Hyperparameters
-    batch_size = 16
-    lr = 0.01
-    momentum = 0.9
-    weight_decay = 1e-3
-    opt_method = 'SGD_momentum'
     image_size = 320
 
-    hps = {'opt_method': opt_method, 'lr': lr, 'momentum': momentum, 'weight_decay': weight_decay}
-    print('Batch size: {}, '.format(batch_size)+', '.join([hp+': '+str(value) for hp, value in hps.items()]))
+    # Hyperparameters
+    batch_size = args.bs
+    hps = None
 
     # Model
     print('==> Building model..')
     net = DeConvNet()
     net = net.to(device)
-    
+
     criterion = nn.CrossEntropyLoss()
     optimizer = None
 
@@ -45,16 +42,14 @@ def main():
     if device == 'cuda':
         cudnn.benchmark = True
         print('cudnn benchmark enabled!')
-        
-    if is_resume:
-        # Load checkpoint.
-        print('==> Resuming from checkpoint..')
-        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-        checkpoint = torch.load('./checkpoint/training_saved.t7') # Load your saved model
-        net.load_state_dict(checkpoint['net'])
-        best_acc = checkpoint['acc']
-        start_epoch = checkpoint['epoch']
-        
+
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/training_saved.t7')  # Load your saved model
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+
     # Data
     print('==> Preparing data..')
     img_transform = transforms.Compose([
@@ -62,22 +57,23 @@ def main():
         transforms.Normalize((0.6672,  0.5865,  0.5985), (1.0, 1.0, 1.0)),
     ])
 
-    data_dir = './data/' # specify the data directory if needed
-    fn = '-'.join([hp+str(value) for hp, value in hps.items()])+'.txt'
-    all_indices = np.loadtxt('./split_indices/'+fn, dtype=str)
-    test_indices = all_indices[-5000:]
+    data_dir = './data/'  # specify the data directory if you like
+
+    all_indices = np.loadtxt('./split_indices/all_indices.txt', dtype=str)
+    size = len(all_indices)
+    test_indices = all_indices[-int(size/10):]
 
     testset = CellImages(data_dir, test_indices, img_transform=img_transform)
     print('Testset size: {}. Number of mini-batch: {}'.format(len(testset), math.ceil(len(testset)/batch_size)))
 
-    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=8)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=args.cpus)
 
     # Evaluation
-
     print('==> Evaluation begins..')
     start_time = time.time()
-    test_results = test(epoch, device, testloader, net, criterion, optimizer, image_size, best_acc, hps, is_save=False, is_print_mb=False, is_savepred=True)
-    best_acc = test_results[-1]
+
+    test_results = test(epoch, device, testloader, net, criterion, optimizer, image_size, best_acc, hps, is_save=False, is_print_mb=False, is_savepred=args.savepred)
+
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
