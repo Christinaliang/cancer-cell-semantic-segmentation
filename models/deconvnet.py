5d1078517c809e8bc191d7af68abbcea81d27c1c
Accelerator <wc2476@columbia.edu>
2018-07-20 16:24:25 +0800

    Update deconvnet.py

--- models/deconvnet.py
+++ models/deconvnet.py
@@ -1,33 +1,7 @@
 import torch
 import torch.nn as nn
 
-
-def ConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, bn=True, leaky=False):
-    layers = [nn.Conv2d(i, o, kernel_size=kernel_size, stride=stride, padding=padding, bias=not bn)]
-
-    if bn:
-        layers += [nn.BatchNorm2d(o)]
-
-    if leaky:
-        layers += [nn.LeakyReLU(inplace=True)]
-    else:
-        layers += [nn.ReLU(inplace=True)]
-
-    return nn.Sequential(*layers)
-
-
-def DeConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, bn=True, leaky=False):
-    layers = [nn.ConvTranspose2d(i, o, kernel_size=kernel_size, stride=stride, padding=padding, bias=not bn)]
-
-    if bn:
-        layers += [nn.BatchNorm2d(o)]
-
-    if leaky:
-        layers += [nn.LeakyReLU(inplace=True)]
-    else:
-        layers += [nn.ReLU(inplace=True)]
-
-    return nn.Sequential(*layers)
+from models.utils import *
 
 
 class DeConvNet(nn.Module):
