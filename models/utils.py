bf6f5448375593c4e1a53ad4d707e4904af81fb8
Accelerator <wc2476@columbia.edu>
2018-10-17 16:25:12 -0400

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 
-def ConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, bn=True, relu=True, bias=True):
-    layers = [nn.Conv2d(i, o, kernel_size=kernel_size, stride=stride, padding=padding, bias=not bn)]
+def ConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, groups=1, bn=True, relu=True):
+    layers = [nn.Conv2d(i, o, kernel_size=kernel_size, stride=stride,
+                        padding=padding, groups=groups, bias=not bn)]
 
     if bn:
         layers += [nn.BatchNorm2d(o)]
@@ -14,9 +15,9 @@ def ConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, bn=True, relu=True
     return nn.Sequential(*layers)
 
 
-def DeConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, bn=True, relu=True, bias=True):
+def DeConvBNReLU(i, o, kernel_size=(3, 3), stride=1, padding=1, groups=1, bn=True, relu=True):
     layers = [nn.ConvTranspose2d(i, o, kernel_size=kernel_size,
-                                 stride=stride, padding=padding, bias=not bn)]
+                                 stride=stride, padding=padding, groups=groups, bias=not bn)]
 
     if bn:
         layers += [nn.BatchNorm2d(o)]
